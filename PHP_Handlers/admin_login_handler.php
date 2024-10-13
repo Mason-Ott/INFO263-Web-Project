0179c6fce2cdbce98019a0de8c19bbf6c270c151
Amber Hu <amber456838@gmail.com>
2024-10-13 02:06:17 -0500

    Added comments for php logic for admin user management system

--- PHP_Handlers/admin_login_handler.php
+++ PHP_Handlers/admin_login_handler.php
@@ -1,15 +1,19 @@
 <?php
 session_start();
 
+// We just have one user for this example
 $username_to_password_map = [
     'admin' => 'admin'
 ];
 
+// Check for login attempt
 if (isset($_POST['username']) && isset($_POST['password'])) {
     $username = $_POST['username'];
     $password = $_POST['password'];
 
+    // Check if the username and password are correct
     if (array_key_exists($username, $username_to_password_map) && $username_to_password_map[$username] === $password) {
+        // Redirect to admin user management page
         header('Location: user_management.php');
         exit();
     } else {
--- PHP_Handlers/user_management_handler.php
+++ PHP_Handlers/user_management_handler.php
@@ -1,17 +1,21 @@
 <?php
 
+// Function to sanitize input
 function sanitize_input($input) {
     return htmlspecialchars(stripslashes(trim($input)));
 }
 
 session_start();
 
+// Check if user clicked the logout button
 if (isset($_POST['logout'])) {
     session_destroy();
+    // Return to the login page
     header('Location: admin_login.php');
     exit();
 }
 
+// Check what value the user wants to update
 if (isset($_POST['vehicle_category'], $_POST['rate_cost_type'], $_POST['value'])) {
     $vehicle_category = sanitize_input($_POST['vehicle_category']);
     $rate_cost_type = sanitize_input($_POST['rate_cost_type']);
@@ -23,7 +27,8 @@ if (isset($_POST['vehicle_category'], $_POST['rate_cost_type'], $_POST['value'])
             'rate_cost_type' => $rate_cost_type,
             'value' => $value
         ];
-
+        
+        // Setup the database query to update the record
         $sql = "UPDATE costs_and_rates SET " . $parameters['rate_cost_type'] .  " = :value WHERE vehicle_category = :vehicle_category";
 
         // Prepare the SQL statement
@@ -31,11 +36,12 @@ if (isset($_POST['vehicle_category'], $_POST['rate_cost_type'], $_POST['value'])
 
         // Bind parameters
         $statement->bindParam(':vehicle_category', $parameters['vehicle_category'], PDO::PARAM_STR);
-        // $statement->bindParam(':rate_cost_type', $parameters['rate_cost_type'], PDO::PARAM_STR);
         $statement->bindParam(':value', $parameters['value'], PDO::PARAM_INT);
 
         // Execute the SQL statement
         $response = $statement->execute();
+
+        // Check if the update was successful
         if ($response) {
             echo '<script>alert("Update Successful!!!")</script>';
         } else {
@@ -46,6 +52,7 @@ if (isset($_POST['vehicle_category'], $_POST['rate_cost_type'], $_POST['value'])
         exit();
     }
 
+    // Redirect to the same page to refresh the data
     header('Location: ' . $_SERVER['PHP_SELF']);
     exit();
 }
